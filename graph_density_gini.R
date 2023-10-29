1c3a2bb482955d250efc56bee3b7392e1e4947db
Luiz Henrique <sbluizhenrique@gmail.com>
2023-10-29 12:48:35 -0300

    uptade

--- graph_density_gini.R
+++
@@ -1,8 +0,0 @@
-# Grafico simples GINI
-
-df |>
-  ggplot() +
-  aes(x = gini, group = ano) +
-  geom_density(fill = "#12E327", alpha = .7, outline.type = "both") +
-  facet_wrap(facets = vars(ano)) +
-  geom_vline(xintercept = .5, linetype = "dashed")
